a4f65109801a396738561d8bf919b911f9d30991
Cihat SALİK <57585087+cihat@users.noreply.github.com>
2024-01-03 01:12:09 +0300

    Add url crate and `has_custom_protocol` func

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::collections::HashSet;
+use url::Url;
+
+// const DATA_URL_DEFAULT_MIME_TYPE: &'static str = "text/plain";
+// const DATA_URL_DEFAULT_CHARSET: &'static str = "us-ascii";
+
+fn has_custom_protocol(url_string: &str) -> bool {
+  let supported_protocols: HashSet<&str> = ["https", "http", "file"].iter().cloned().collect();
+
+  match Url::parse(url_string) {
+    Ok(url) => supported_protocols.contains(url.scheme()),
+    Err(_) => false,
+  }
+}
+
 fn main() {
-  println!("Hello, world!");
+  let url_string = "https://example.com";
+  let result = has_custom_protocol(url_string);
+  println!("Does the URL have a custom protocol? {}", result);
 }
